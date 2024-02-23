b9c48d3b8646318523800925110ac725546d5ff6
basit-devBE <basitmohammed362@gmail.com>
2024-02-23 13:00:57 +0000

    hello

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,12 @@
-# package resource for Flask using pip3
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
-
-# install pip3 if not already installed:
-
+# Install pip3 if not already installed
 package { 'python3-pip':
   ensure => 'installed',
 }
 
-# Ensure pip3 is installed, and then install Flask with the specified version.
-class { 'python3-pip':
-  ensure => 'installed',
-} -> package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Execute pip3 to install Flask with the specified version
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/bin',
+  unless  => '/usr/bin/pip3 show Flask | grep Version | grep -q 2.1.0',
+  require => Package['python3-pip'],
 }
