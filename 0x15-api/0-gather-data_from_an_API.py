bcfd0d25b70de0f3f971bd2feb15abaac200155d
basit-devBE <basitmohammed362@gmail.com>
2024-03-25 14:52:25 +0000

    hello

--- 0x15-api/0-gather-data_from_an_API.py
+++ 0x15-api/0-gather-data_from_an_API.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""
-    A python script that uses the urlib or request module to fetch data from an API
-    """
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-if __name__== "__main__":
-    url = "hhtps:jsonplaceholder.typicode.com/users"
-    response = requests.get(url + "users/{}".format(sys.argv[1])).json()
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [task.get("title") for task in todos if task.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(response.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(task)) for task in completed]
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
     
